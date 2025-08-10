64081e39a076112e0533039f8df87e97a7455867
Ali <alibeisenov5@gmail.com>
2025-08-10 16:24:21 +0500

    Добавил backend как часть основного репозитория

---
+++ backend/main.py
@@ -0,0 +1,64 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, EmailStr
+from passlib.context import CryptContext
+from jose import jwt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
+ # test
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://kokonaihub.onrender.com/"],  # адрес фронтенда
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+import os
+
+load_dotenv()  # загружаем переменные из .env
+SECRET_KEY = os.getenv("SECRET_KEY")
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY не установлен в переменных окружения!")
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+fake_users_db = {}
+
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+def create_access_token(data: dict, expires_delta: timedelta = None):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=15))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+@app.post("/api/register")
+async def register(user: UserCreate):
+    if user.email in fake_users_db:
+        raise HTTPException(status_code=400, detail="Пользователь уже существует")
+    hashed_password = get_password_hash(user.password)
+    fake_users_db[user.email] = {"email": user.email, "hashed_password": hashed_password}
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    return {"token": access_token}
+
+@app.post("/api/login")
+async def login(user: UserCreate):
+    db_user = fake_users_db.get(user.email)
+    if not db_user or not verify_password(user.password, db_user["hashed_password"]):
+        raise HTTPException(status_code=400, detail="Неверный email или пароль")
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    return {"token": access_token}
+
