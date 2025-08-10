4a5ff78e9cdad947ea61614b726abbbd87e0f39b
Ali <alibeisenov5@gmail.com>
2025-08-10 23:16:52 +0500

    test 2

--- my-react-app/src/auth/isAuthenticated.jsx
+++ my-react-app/src/auth/isAuthenticated.jsx
@@ -1,10 +1,10 @@
-// import jwt_decode from "jwt-decode";
-
-export default function isAuthenticated() {
+// src/auth/isAuthenticated.jsx
+export async function isAuthenticated() {
   const token = localStorage.getItem("token");
   if (!token) return false;
 
   try {
+    const jwt_decode = (await import('jwt-decode')).default; // динамический импорт, если надо
     const { exp } = jwt_decode(token);
     if (!exp || Date.now() >= exp * 1000) {
       localStorage.removeItem("token");
