df683110b7bfbdecf8d520457c51cac9321d8eba
Ali <alibeisenov5@gmail.com>
2025-08-10 16:48:09 +0500

    modified:   backend/main.py

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://kokonaihub.onrender.com/"],  # адрес фронтенда
+    allow_origins=["https://kokonaihub.onrender.com"],  # адрес фронтенда
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
