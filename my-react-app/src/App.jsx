2daaf9af6d06927d6e2e485c4c592995be63c8ca
Ali <alibeisenov5@gmail.com>
2025-08-11 20:51:13 +0500

    edited more

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,75 +7,109 @@ import BottomMenu from './components/BottomMenu';
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Profile from "./pages/Profile";
+import Error from "./pages/Error";
 import PrivateRoute from "./auth/PrivateRoute"
 import { isAuthenticated } from "./auth/isAuthenticated";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useLocation } from 'react-router-dom';
+
 
 
 import {HomeIcon,Archive,User,Settings,Github,LogOut } from "lucide-react";
 
 function AppWrapper() {
+  const token = localStorage.getItem("token");
+  const location = useLocation();
   const navigate = useNavigate();
   const items = [
     { icon: <HomeIcon color="#7C3AED" size={18} />, label: 'Home', onClick: () => navigate("/home") },
     { icon: <Archive color="#7C3AED" size={18} />, label: 'Archive', onClick: () => alert('Archive!') },
     { icon: <User  color="#7C3AED" size={18} />, label: 'Profile', onClick: async () => {
-  const auth = await isAuthenticated();
-  if (auth) {
-    console.log("Все ок");
-    toast.success("Успешно!", {
-  className: "bg-green-600 text-white font-bold",
-  bodyClassName: "text-sm",
-  progressClassName: "bg-white"
-});
+        const auth = await isAuthenticated();
+        if (auth) {
+          // console.log("Все ок");
+          //   toast.success("Успешно!");
 
-    navigate("/profile");
-  } else {
-    console.log(auth)
-    toast.error("Сначала авторизуйтесь");
-    navigate("/login");
-  }
-},},
+          navigate("/profile");
+        } else {
+          console.log(auth)
+          toast.error("Сначала авторизуйтесь");
+          navigate("/login");
+        }
+      },
+    },
     { icon: <Settings color="#7C3AED" size={18} />, label: 'Settings', onClick: () => alert('Settings!') },
     { icon: <Github  color="#7C3AED"size={18} />, label: 'Github', onClick: () => window.open('https://github.com/kaiirashoovt') },
-    {
-      icon: <LogOut color="#7C3AED" size={18} />,
-      label: 'LogOut',
-      onClick: async () => {
-        try {
-          const token = localStorage.getItem("token");
-          if (!token) throw new Error("Токен не найден");
+    // {
+    //   icon: <LogOut color="#7C3AED" size={18} />,
+    //   label: 'LogOut',
+    //   onClick: async () => {
+    //     try {
+    //       const token = localStorage.getItem("token");
+    //       if (!token) throw new Error("Токен не найден");
 
-          const response = await fetch("https://my-fastapi-backend-f4e2.onrender.com/api/logout", {
-            method: "POST",
-            headers: {
-              "Authorization": `Bearer ${token}`,
-              "Content-Type": "application/json"
-            }
-          });
+    //       const response = await fetch("https://my-fastapi-backend-f4e2.onrender.com/api/logout", {
+    //         method: "POST",
+    //         headers: {
+    //           "Authorization": `Bearer ${token}`,
+    //           "Content-Type": "application/json"
+    //         }
+    //       });
 
-          if (!response.ok) {
-            // Можно выдать сообщение об ошибке, если нужно
-            console.warn("Ошибка при logout:", response.status);
-          }
-        } catch (error) {
-          console.error("Ошибка logout:", error);
-        } finally {
-          // Удаляем токен и перенаправляем вне зависимости от результата fetch
-          localStorage.removeItem("token");
-          navigate("/login")
-          toast.info("Вы вышли!", {
-            className: "bg-green-600 text-white font-bold",
-            bodyClassName: "text-sm",
-            progressClassName: "bg-white"
-          });
+    //       if (!response.ok) {
+    //         // Можно выдать сообщение об ошибке, если нужно
+    //         console.warn("Ошибка при logout:", response.status);
+    //       }
+    //     } catch (error) {
+    //       console.error("Ошибка logout:", error);
+    //     } finally {
+    //       // Удаляем токен и перенаправляем вне зависимости от результата fetch
+    //       localStorage.removeItem("token");
+    //       navigate("/login")
+    //       toast.info("Вы вышли!", {
+    //         className: "bg-green-600 text-white font-bold",
+    //         bodyClassName: "text-sm",
+    //         progressClassName: "bg-white"
+    //       });
 
-        }
-      }
-    }
+    //     }
+    //   }
+    // }
 
   ];
+
+  if (token) {
+  items.push({
+    icon: <LogOut color="#7C3AED" size={18} />,
+    label: "LogOut",
+    onClick: async () => {
+      try {
+        const response = await fetch("https://my-fastapi-backend-f4e2.onrender.com/api/logout", {
+          method: "POST",
+          headers: {
+            "Authorization": `Bearer ${token}`,
+            "Content-Type": "application/json"
+          }
+        });
+
+        if (!response.ok) {
+          console.warn("Ошибка при logout:", response.status);
+        }
+      } catch (error) {
+        console.error("Ошибка logout:", error);
+      } finally {
+        localStorage.removeItem("token");
+        navigate("/login");
+        toast.info("Вы вышли!", {
+          className: "bg-green-600 text-white font-bold",
+          bodyClassName: "text-sm",
+          progressClassName: "bg-white"
+        });
+      }
+    }
+  });
+}
 
   return (
     <div >
@@ -88,23 +122,19 @@ function AppWrapper() {
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route
-            path="/profile"
-            element={
-              <PrivateRoute>
-                <Profile />
-              </PrivateRoute>
-            }
-          />
+          <Route path="/error" element={<Error />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
-              <BottomMenu 
-        className="fixed bottom-0  z-50 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white"
-        items={items}
-        panelHeight={68}
-        baseItemSize={50}
-        magnification={70}
-        />
-        <ToastContainer
+        {location.pathname !== '/login' && location.pathname !== '/register' && (
+          <BottomMenu 
+            className="fixed bottom-0 z-50 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 text-white"
+            items={items}
+            panelHeight={68}
+            baseItemSize={50}
+            magnification={70}
+          />
+        )}
+<ToastContainer
   position="bottom-right"
   autoClose={3000}
   hideProgressBar={false}
@@ -113,13 +143,9 @@ function AppWrapper() {
   pauseOnFocusLoss
   draggable
   pauseOnHover
-  theme="colored"
 />
 
-
-        
       </main>
-
     </div>
   );
 }
