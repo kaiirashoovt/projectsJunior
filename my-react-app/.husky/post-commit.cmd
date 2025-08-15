ca08d144d59292e6588b8f75c6aaad0609a35fb6
Ali <alibeisenov5@gmail.com>
2025-08-15 09:12:19 +0500

    Тест автогенерации

--- my-react-app/.husky/post-commit.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-REM Получаем сообщение последнего коммита
-for /f "delims=" %%i in ('git log -1 --pretty=%%B') do set COMMIT_MSG=%%i
-
-REM Запускаем генерацию обновлений
-node generateUpdates.cjs "%COMMIT_MSG%"
-
-REM Добавляем src/updates.jsx в индекс
-git add src/updates.jsx
