7daab44eee22a81cde6eaa41f997ef759cfe905f
Ali <alibeisenov5@gmail.com>
2025-08-15 09:13:03 +0500

    Тест автогенерации

--- my-react-app/src/updates.jsx
+++ my-react-app/src/updates.jsx
@@ -1,9 +1,37 @@
-#!/bin/sh
-# Получаем сообщение последнего коммита
-COMMIT_MSG=$(git log -1 --pretty=%B)
-
-# Запускаем Node.js скрипт
-node generateUpdates.cjs "$COMMIT_MSG"
-
-# Добавляем src/updates.jsx в индекс
-git add src/updates.jsx
+export const updates = [
+  {
+    "date": "2025-08-15",
+    "version": "v1.0.5",
+    "changes": [
+      "%COMMIT_MSG%"
+    ]
+  },
+  {
+    "date": "2025-08-15",
+    "version": "v1.0.4",
+    "changes": [
+      "%COMMIT_MSG%"
+    ]
+  },
+  {
+    "date": "2025-08-15",
+    "version": "v1.0.3",
+    "changes": [
+      "Тест автогенерации"
+    ]
+  },
+  {
+    "date": "2025-08-15",
+    "version": "v1.0.2",
+    "changes": [
+      "Твой коммит сообщение"
+    ]
+  },
+  {
+    "date": "2025-08-15",
+    "version": "v1.0.1",
+    "changes": [
+      "Изменения"
+    ]
+  }
+];
