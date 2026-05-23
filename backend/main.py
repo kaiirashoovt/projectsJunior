eadce005cc233e63f57dbd93c71fcdd460e6ddac
Ali <alibeisenov5@gmail.com>
2026-05-24 00:14:44 +0500

    currently on branch main Your branch is ahead of 'origin/main' by 4 commits.   (use "git push" to publish your local commits)

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # адрес фронтенда
+    allow_origins=["https://kokonai-hub.vercel.app", "http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
